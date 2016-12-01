f957ca7200144bfe0a5412ab5284b48d7ea3fa8f
Ravi Kumar Singh <ravi.kumar.singh@kex-ag.com>
2016-12-01 20:58:55 +0100

    add rake algorithm

--- src/main/java/de/rwth/i9/cimt/service/kpextraction/RAKEKPExtraction.java
+++ src/main/java/de/rwth/i9/cimt/service/kpextraction/RAKEKPExtraction.java
@@ -1,10 +1,8 @@
 package de.rwth.i9.cimt.service.kpextraction;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.solr.client.solrj.SolrServerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +11,6 @@ import org.springframework.stereotype.Service;
 import de.rwth.i9.cimt.model.Keyword;
 import de.rwth.i9.cimt.service.nlp.opennlp.OpenNLPImpl;
 import de.rwth.i9.cimt.test.AppRake;
-import uk.ac.shef.dcs.jate.JATEException;
-import uk.ac.shef.dcs.jate.model.JATETerm;
 
 @Service("rakeKPExtraction")
 public class RAKEKPExtraction {
@@ -25,29 +21,15 @@ public class RAKEKPExtraction {
 
 	public List<Keyword> extractKeywordRAKE(String text, int numKeywords) {
 		List<Keyword> keywords = new ArrayList<Keyword>();
+		List<Keyword> totalKeywords = new ArrayList<Keyword>();
 		int keywordCount = 0;
-		try {
-			List<JATETerm> terms = AppRake.rakeAlgo(text);
-			String termString = "";
-			for (JATETerm term : terms) {
-				termString += term.getString() + "\t" + term.getScore() + "\n";
-				Keyword keyword = new Keyword();
-				keyword.setKeyword(term.getString());
-				keyword.setScore(term.getScore());
-				keywords.add(keyword);
-				if (++keywordCount > numKeywords)
-					break;
-			}
-		} catch (JATEException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SolrServerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		totalKeywords = AppRake.rakeAlgo(text, openNLPImpl);
+		for (Keyword keyword : totalKeywords) {
+			keywords.add(keyword);
+			if (++keywordCount > numKeywords)
+				break;
 		}
+
 		return keywords;
 	}
 
